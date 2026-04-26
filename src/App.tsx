ec9841938d0e341b37adc0a0cde71779d0423a67
pradeep <pradeepkumawat1310@gmail.com>
2026-04-26 23:15:37 +0530

    final update

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter>npm install
         <SocialSticky />
         <DonateSticky />
         <Routes>
